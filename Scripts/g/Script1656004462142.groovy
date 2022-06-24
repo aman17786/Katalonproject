41db57a28c7394d4a34ceb354eae05f5c7426be5
aman17786 <amanbir.seth1709@gmail.com>
2022-06-24 09:12:28 -0400

    l

--- Scripts/g/Script1656004462142.groovy
+++ Scripts/g/Script1656004462142.groovy
@@ -17,3 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.check(findTestObject(null))
+
+WebUI.delay(0)
+
